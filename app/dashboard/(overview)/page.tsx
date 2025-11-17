410aa7769fcf873df03dcb8e7c0e425a6643023f
solomon54 <solomontsehay54@gmail.com>
2025-11-17 16:46:44 +0300

    adding streeming function for both invoice and card section.

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,19 +1,22 @@
+import CardWrapper from '@/app/ui/dashboard/cards';
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData} from '@/app/lib/data';
+
 import { Suspense } from 'react';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
-import {RevenueChartSkeletone} from '@/app/ui/skeletons';
+import { RevenueChartSkeleton , LatestInvoicesSkeleton, CardsSkeleton} from '@/app/ui/skeletons';
+import { fetchCardData } from '@/app/lib/data';
+
  
 export default async function Page() {
-  // const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  // const latestInvoices = await LatestInvoices();
+
   const cardData= await fetchCardData();
   const { numberOfCustomers, numberOfInvoices, totalPaidInvoices, totalPendingInvoices } = cardData;
+  
   return (
-    <main>
+    <main> 
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
@@ -24,11 +27,18 @@ export default async function Page() {
         <Card title="Total Customers" value={numberOfCustomers} type="customers" />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-       < suspense falback={(<RevenueChartSkeleton />)}>
+       <Suspense fallback={(<RevenueChartSkeleton />)}>
         <RevenueChart />
-        </ Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        </Suspense>
+
+        <Suspense fallback={(<LatestInvoicesSkeleton />)}>
+       <LatestInvoices />
+        </Suspense>
+
+        <Suspense fallback={<CardsSkeleton />}>
+          <CardWrapper />
+        </Suspense>
       </div>
-    </main>
+     </main>
   );
 }
